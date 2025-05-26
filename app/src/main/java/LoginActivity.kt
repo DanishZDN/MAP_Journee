f1263dfeb4ea070317b0d5d77f05506be2fc08cd
Dimas.Bagus <dimasbagus.dbn@gmail.com>
2025-05-26 14:18:42 +0700

    Update LoginActivity.kt

--- app/src/main/java/LoginActivity.kt
+++ app/src/main/java/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.android.example.mobileapp_journeeg9.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
